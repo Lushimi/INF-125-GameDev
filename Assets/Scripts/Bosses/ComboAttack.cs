24a5063f6f38c654d6496d8624cc3d619974a4f8
Tenebranas <bloodsteelreviews@gmail.com>
2021-11-16 16:27:40 -0800

    fixed bug - the boss can still finish his attack even after he's dead

--- Assets/Scripts/Bosses/ComboAttack.cs
+++ Assets/Scripts/Bosses/ComboAttack.cs
@@ -9,6 +9,8 @@ public class ComboAttack : MonoBehaviour
     public Transform attackPoint => transform.Find("MeleeAttackPoint");
     public LayerMask enemyLayers;
 
+    public float health => transform.GetComponent<BossData>().currentHealth;
+
     public int attackDamage = 5;
     public float attackRangeNormal = 1.5f;
     public float finalAttackRange = 2.5f;
@@ -45,7 +47,7 @@ public class ComboAttack : MonoBehaviour
     public IEnumerator IComboAttack()
     {
         yield return new WaitForSeconds(startup);
-        attack();
+        if(!isDead()) attack();
         yield return new WaitForSeconds(firstmovelag);
         animator.SetBool("isMeleeAttacking", false);
         StartCoroutine(MoveTowardsPlayer(firstmovetracking,1));
@@ -106,16 +108,24 @@ public class ComboAttack : MonoBehaviour
     //Movement
     public IEnumerator MoveTowardsPlayer(float tracking_time, int attackType)
     {
-        gameObject.GetComponent<BossControl>().movement_override = true;
-        gameObject.GetComponent<Pathfinding.AIPath>().canMove = true;
-        float oldSpeed = gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed;
-        gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed = 10;
-        yield return new WaitForSeconds(tracking_time);
-        gameObject.GetComponent<Pathfinding.AIPath>().canMove = false;
-        gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed = oldSpeed;
-        gameObject.GetComponent<BossControl>().movement_override = false;
-        if (attackType == 1) attack();
-        else if (attackType == 2) attackFinal();
+        if(!isDead())
+        {
+            gameObject.GetComponent<BossControl>().movement_override = true;
+            gameObject.GetComponent<Pathfinding.AIPath>().canMove = true;
+            float oldSpeed = gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed;
+            gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed = 10;
+            yield return new WaitForSeconds(tracking_time);
+            gameObject.GetComponent<Pathfinding.AIPath>().canMove = false;
+            gameObject.GetComponent<Pathfinding.AIPath>().maxSpeed = oldSpeed;
+            gameObject.GetComponent<BossControl>().movement_override = false;
+            if (attackType == 1) attack();
+            else if (attackType == 2) attackFinal();
+        }
+    }
+
+    public bool isDead()
+    {
+        return health <= 0;
     }
 }
 
